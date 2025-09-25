a01e89ed55f14cbc5bf3a976fce4ad8a3259df3b
tokenbender <ahm.rimer@gmail.com>
2025-09-25 13:26:27 +0530

    Raise token budget to 9B and tweak 30M batch

--- experiments/pretrain/avatarl_30m/config.py
+++ experiments/pretrain/avatarl_30m/config.py
@@ -28,7 +28,7 @@ wandb_dir = str(_CONFIG_DIR)
 # data
 dataset = "openwebtext"
 gradient_accumulation_steps = 8
-batch_size = 64
+batch_size = 32
 block_size = 1048
 
 # model (≈30M params)
@@ -42,7 +42,7 @@ bias = False
 learning_rate = 6e-4
 max_iters = None
 max_epochs = None
-max_tokens = 900_000_000
+max_tokens = 9_000_000_000
 weight_decay = 1e-1
 beta1 = 0.9
 beta2 = 0.95
